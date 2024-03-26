42f9b38028ed7a54258cf28b86f4227a290d811d
Evan Huang <huangguanhao@youloft.com>
2024-03-26 14:46:57 +0800

    :art: 请求拦截注入token

--- src/util/http.js
+++ src/util/http.js
@@ -1,23 +1,30 @@
 /**
  * @Author: Hgh
  * @Date: 2024-03-04 16:32:17
- * @LastEditTime: 2024-03-04 16:36:02
+ * @LastEditTime: 2024-03-26 14:45:35
  * @LastEditors: Hgh
  * @Description:
  */
 import axios from "axios";
+import { getToken } from './token'
 
 const http = axios.create({
     baseURL: 'http://geeek.iteheima.net/v1_0',
     timeout: 5000,
 })
 
+// 请求拦截器
 http.interceptors.request.use((config) => {
+    const token = getToken()
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`
+    }
     return config
 }, (error) => {
     return Promise.reject(error)
 })
 
+// 响应拦截器
 http.interceptors.response.use((response) => {
     return response
 }, (error) => {
